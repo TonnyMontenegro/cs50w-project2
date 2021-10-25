5c3e3679e19d30ba94656801e85b0377f40b0a99
TonnyMontenegro <tonny.n1105@gmail.com>
2021-10-25 05:09:43 +0000

    avance #1 de interfaz(login completo y semi ruteo)

--- application.py
+++ application.py
@@ -1,6 +1,23 @@
 import os
 
-from flask import Flask, render_template,request
+'''
+Deque (Doubly Ended Queue) in Python is implemented using the module "collections".
+Deque is preferred over list in the cases where we need quicker append and pop operations
+from both the ends of container, as deque provides an O(1) time complexity for append and
+pop operations as compared to list which provides O(n) time complexity.
+'''
+from collections import deque
+
+'''
+functools is a standard Python module for higher-order functions
+(functions that act on or return other functions). wraps() is a decorator
+that is applied to the wrapper function of a decorator. It updates the
+wrapper function to look like wrapped function by copying attributes such
+as __name__, __doc__ (the docstring), etc.
+'''
+from functools import wraps
+
+from flask import Flask, render_template, request, session, redirect, flash, url_for
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
@@ -8,17 +25,77 @@ app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
 
+
 '''Local Storage'''
-Users={}
-Chanels={}
-Messages={}
+Users=[]
+Channels=[]
+Messages= dict()
+
+Channels.append('General')
+Channels.append('Xda')
+Channels.append('Xda')
+Channels.append('Xda')
+Channels.append('Xda')
+Channels.append('Xda')
+Channels.append('Xda')
+Channels.append('Xda')
+Channels.append('Xda')
+Channels.append('Xda')
+Messages['General']=deque()
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("username") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function
 
 @app.route("/")
+@login_required
 def index():
-    session= False
     return render_template("index.html")
 
-@app.route("/main")
-def main():
-    session= True
-    return render_template("main.html")
+@app.route("/login", methods=['GET','POST'])
+def login():
+    session.clear()
+    username = request.form.get("username")
+
+    if request.method == "GET":
+        return render_template("login.html")
+    else:
+        if( len(username) < 3 or username == ''):
+            flash("Error, Nombre de usuario no valido,verifique su usuario")
+            return render_template("login.html")
+        else:
+            Users.append(username)
+            session['username']=username
+            session.permanet=True
+            session["Auth"]= True
+
+            return redirect("/channels/General")
+
+@app.route("/logout", methods=['GET','POST'])
+@login_required
+def logout():
+    # se limpia la sesion del usuario
+    Users.remove(session['username'])
+    session.clear()
+    # se le notifica al usuario y se le redirije a la pagina de inicio de sesion
+    flash("Sesion cerrada")
+    return render_template("login.html")
+
+@app.route("/channels/<channel>", methods=['GET','POST'])
+@login_required
+def channels_list(channel):
+    user = session['username']
+    session['open_channel']=channel
+
+    if channel not in Channels:
+        return redirect("/channels/General")
+    else:
+        if request.method == "POST":
+            return redirect("/")
+        else:
+            return render_template("channels.html",user=user,users=Users,channels=Channels,messages=Messages[channel],channel=session['open_channel'])
+            
---
+++ static/system.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', ()=> {
+
+    const socket = new WebSocket("http://localhost:8080");
+
+    let User = localStorage.getItem("Username")
+});
